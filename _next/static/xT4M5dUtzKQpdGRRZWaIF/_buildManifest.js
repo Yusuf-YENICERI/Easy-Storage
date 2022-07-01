2e8f7b92529e4278a1ea79da5ef1cb802b6e4dbe
“Yusuf-YENICERI” <joshard20917@gmail.com>
2022-07-01 19:51:34 -0400

    Some changes made

---
+++ _next/static/xT4M5dUtzKQpdGRRZWaIF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/685-f4ba8319e25b63e4.js","static/chunks/pages/index-1854d40f5e15d6b2.js"],"/_error":["static/chunks/pages/_error-a3f18418a2205cb8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
